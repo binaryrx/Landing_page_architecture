daff454445d2fb8da06bfa7d0109dab9e0b6b508
BinaryRx <BinaryRx@gmail.com>
2019-01-20 18:14:05 +0200

    CHanges

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+/*!
+ * landing_Page v0.0.1
+ * Shachar Parnas Architechts Landing Page
+ * (c) 2019 YOUR NAME
+ * MIT License
+ * https://github.com/binaryrx/Landing_page_architecture
+ */
+
+console.log('main.js file loaded');
